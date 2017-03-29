ec1f107788438787bbbd4463d527431b693495d7
manthanthakker <manthanthakker40@gmail.com>
2017-03-29 03:42:04 -0400

    upload

--- OnlineBankingSystem/src/onlinebankingsystem/OnlineBankingSystem.java
+++ OnlineBankingSystem/src/onlinebankingsystem/OnlineBankingSystem.java
@@ -26,18 +26,19 @@ public class OnlineBankingSystem {
      static Person person=null;
      static Customer customer=null;
      static Manager manager1=null;
-     static Account sacc=null;
-     static Account cacc=null;
-     static Loan sav_acc=null;
+     
+     static CheckingAccount check_acc=null;
+     static SavingsAccount sav_acc=null;
      static EducationLoan educationlocal=null;
      static HomeLoan homeloan=null;
      static CarLoan carloan=null;
      static Manager manager=null;
-     static Card card=null;
+     
      static DebitCard dcard=null;
      static CreditCard ccard=null;
      static Connection c=null;
      
+     /// ----- COMPLETED
     public static void populatecustomer(int cust_id,Connection c)
     {
             try {
@@ -47,9 +48,9 @@ public class OnlineBankingSystem {
             PreparedStatement st2=c.prepareStatement("Select * from Customer where cust_id=? ");
             st2.setInt(1, cust_id);
             ResultSet rs2=st2.executeQuery();
-            PreparedStatement st3=c.prepareStatement("Select * from Manager where mid=? ");
-            st3.setInt(1, cust_id);
-            ResultSet rs3=st3.executeQuery();
+         //   PreparedStatement st3=c.prepareStatement("Select * from Manager where mid=? ");
+          //  st3.setInt(1, cust_id);
+           // ResultSet rs3=st3.executeQuery();
             while(rs.next())
             {
                  int c_id=rs.getInt(1);
@@ -60,34 +61,110 @@ public class OnlineBankingSystem {
                  int contact_no=rs.getInt(6);
                  String password=rs.getString(7);
                  String email=rs.getString(8);
+                 
                  person=new Person(c_id,fname,lname,dob,address,contact_no,password,email);
                  
                  
                 System.out.println(person);
             
             } 
-            if(rs2!=null)
+            if(rs2.next())
             {
                 customer=new Customer(rs2.getInt(1));
                 System.out.println(customer);
             }
-            if(rs3!=null)
+          /*  if(rs3.next())
             {
                 manager=new Manager(rs3.getInt(1));
                 System.out.println(manager);
-            }
+            }*/
             } catch (SQLException ex) {
             Logger.getLogger(OnlineBankingSystem.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
     
+    public static void populatesavings(int cust_id,Connection c)
+    {
+        try {
+            PreparedStatement st1=c.prepareStatement("Select a.id,a.account_number,a.balance,a.DateOfStart, a.DateOfEnd, a.status  from  personaccount pa, Account a, SavingsAccount s where pa.personid=? and pa.accid=a.id and a.id=s.sid");
+            //int s_id, int acc_no, double balance, String dos, String doe, String status)
+            st1.setInt(1, cust_id);
+            ResultSet rs=st1.executeQuery();
+            
+            while(rs.next())
+            {
+                 
+                 
+                 sav_acc=new SavingsAccount(rs.getInt(1),rs.getInt(2),rs.getDouble(3),rs.getString(4),rs.getString(5),rs.getString(6));
+                 
+                 
+                System.out.println(sav_acc);
+            
+            } 
+           
+            } catch (SQLException ex) {
+            Logger.getLogger(OnlineBankingSystem.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    // ONGOING
+     public static void populatechecking(int cust_id,Connection c)
+    {
+        try {
+            PreparedStatement st1=c.prepareStatement("Select a.id,a.account_number,a.balance,a.DateOfStart, a.DateOfEnd, a.status  from  personaccount pa, Account a, CheckingAccount s where pa.personid=? and pa.accid=a.id and a.id=s.caid");
+            //int s_id, int acc_no, double balance, String dos, String doe, String status)
+            st1.setInt(1, cust_id);
+            ResultSet rs=st1.executeQuery();
+            
+            while(rs.next())
+            {
+                 
+                 
+                 check_acc=new CheckingAccount(rs.getInt(1),rs.getInt(2),rs.getDouble(3),rs.getString(4),rs.getString(5),rs.getString(6));
+                 
+                 
+                System.out.println(check_acc);
+            
+            } 
+           
+            } catch (SQLException ex) {
+            Logger.getLogger(OnlineBankingSystem.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+     
+     public static void populatecreditcard(int cust_id,Connection c)
+    {
+        try {
+            PreparedStatement st1=c.prepareStatement("select credit_id,maxcredit,card_no,issuedate,expdate,cvv,status from creditcard credit,card c where c.usedby=? and credit.credit_id=c.card_id");
+            //int cid, double maxcredit, double cardno, double issuedate, double expdate, int cvv, String status
+            //int s_id, int acc_no, double balance, String dos, String doe, String status)
+            //nt did, double cardno, String issuedate, String expdate, int cvv, String status
+            st1.setInt(1, cust_id);
+            ResultSet rs=st1.executeQuery();
+            
+            while(rs.next())
+            {
+                 
+                 
+                 ccard=new CreditCard(rs.getInt(1),rs.getDouble(2),rs.getDouble(3),rs.getString(4),rs.getString(5),rs.getInt(6),rs.getString(7));
+                 //int cid, double maxcredit, double cardno, double issuedate, double expdate, int cvv, String status
+                 
+                System.out.println(ccard);
+            
+            } 
+           
+            } catch (SQLException ex) {
+            Logger.getLogger(OnlineBankingSystem.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    } 
     
     public static void main(String[] args) {
        
         
         initialization();
         populatecustomer(1,c);
+       // populatesavings(1,c);
+        populatechecking(1,c);
         Scanner sc=new Scanner(System.in);
         System.out.println(" ONLINE BANKING SYSTEM ");
         System.out.println("Enter Choice");
@@ -236,38 +313,163 @@ class Account
     String status;
     
 }
+
+
+
+
+//// WORKING
+
+
 class SavingsAccount extends Account
 {
     int s_id;
+     
+   
+    @Override
+    public String toString() {
+        return "SavingsAccount{" + "s_id=" + s_id + ", acc_id=" + acc_id + ", acc_no=" + acc_no + ", balance=" + balance + ", dos=" + dos + ", doe=" + doe + ", status=" + status + ", ROI=" + ROI + '}';
+    }
+
+    public SavingsAccount(int s_id, int acc_no, double balance, String dos, String doe, String status) {
+        
+        this.acc_id = s_id;
+        this.s_id=s_id;
+        this.acc_no = acc_no;
+        this.balance = balance;
+        this.dos = dos;
+        this.doe = doe;
+        this.status = status;
+    }
+    
+
+    
     final double ROI=8.5;
 }
+
+
+
+
+
+
+
+
+
+
+
 class CheckingAccount extends Account
 {
     int id;
+  
+
+    @Override
+    public String toString() {
+        return "CheckingAccount{" + "id=" + id + ", acc_id=" + acc_id + ", acc_no=" + acc_no + ", balance=" + balance + ", dos=" + dos + ", doe=" + doe + ", status=" + status + '}';
+    }
+
+    public CheckingAccount(int id, int acc_no, double balance, String dos, String doe, String status) {
+        this.id = id;
+        this.acc_id = acc_id;
+        this.acc_no = acc_no;
+        this.balance = balance;
+        this.dos = dos;
+        this.doe = doe;
+        this.status = status;
+    }
+
+    public CheckingAccount(int id) {
+        this.id = id;
+    }
 }
 
 class Card
 {
     int cid;
     double cardno;
-    double issuedate;
-    double expdate;
+    String issuedate;
+    String expdate;
     int cvv;
     String status;
+    public Card()
+    {
+        
+    }
+
+    public Card(int cid, double cardno, double issuedate, double expdate, int cvv, String status) {
+        this.cid = cid;
+        this.cardno = cardno;
+        this.issuedate = issuedate;
+        this.expdate = expdate;
+        this.cvv = cvv;
+        this.status = status;
+    }
 }
-class DebitCard
+class DebitCard extends Card
 {
     int did;
+    
+  
+
+    @Override
+    public String toString() {
+        return "DebitCard{" + "did=" + did + ", cardno=" + cardno + ", issuedate=" + issuedate + ", expdate=" + expdate + ", cvv=" + cvv + ", status=" + status + '}';
+    }
+
+    public DebitCard(int did, double cardno, String issuedate, String expdate, int cvv, String status) {
+        this.did = did;
+        this.cardno = cardno;
+        this.issuedate = issuedate;
+        this.expdate = expdate;
+        this.cvv = cvv;
+        this.status = status;
+    }
+
+    public DebitCard(int did) {
+        this.did = did;
+    }
 }
-class CreditCard
+class CreditCard extends Card
 {
-    int cid;
+    
+    
     double maxcredit;
+   
+   
+
+    @Override
+    public String toString() {
+        return "CreditCard{" + "cid=" + cid + ", maxcredit=" + maxcredit + ", cardno=" + cardno + ", issuedate=" + issuedate + ", expdate=" + expdate + ", cvv=" + cvv + ", status=" + status + '}';
+    }
+
+    public CreditCard(int cid, double maxcredit, double cardno, String issuedate, String expdate, int cvv, String status) {
+        this.cid = cid;
+        this.maxcredit = maxcredit;
+        this.cardno = cardno;
+        this.issuedate = issuedate;
+        this.expdate = expdate;
+        this.cvv = cvv;
+        this.status = status;
+    }
+
+    
+
+    public CreditCard(int cid, double maxcredit) {
+        this.cid = cid;
+        this.maxcredit = maxcredit;
+    }
 }
 class Loan
 {
    int id;
    double amount;
+
+    public Loan(int id, double amount) {
+        this.id = id;
+        this.amount = amount;
+    }
+    public Loan()
+    {
+        
+    }
    
 }
 class EducationLoan extends Loan
@@ -275,18 +477,36 @@ class EducationLoan extends Loan
    int e_id;
    double roi;
    String university;
+
+    public EducationLoan(int e_id, double roi, String university) {
+        this.e_id = e_id;
+        this.roi = roi;
+        this.university = university;
+    }
 }
 class CarLoan extends Loan
 {
     int c_id;
     double roi;
     String car_model;
+
+    public CarLoan(int c_id, double roi, String car_model) {
+        this.c_id = c_id;
+        this.roi = roi;
+        this.car_model = car_model;
+    }
 }
 class HomeLoan extends Loan
 {
     int h_id;
     double roi;
     String address;
+
+    public HomeLoan(int h_id, double roi, String address) {
+        this.h_id = h_id;
+        this.roi = roi;
+        this.address = address;
+    }
 }
 class Transaction
 {
@@ -295,4 +515,12 @@ class Transaction
     double from_id;
     double amount;
     String type;
+
+    public Transaction(int t_id, double to_id, double from_id, double amount, String type) {
+        this.t_id = t_id;
+        this.to_id = to_id;
+        this.from_id = from_id;
+        this.amount = amount;
+        this.type = type;
+    }
 }
